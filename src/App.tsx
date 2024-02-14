de0e79724100b0d7057d9998c788e2887e30924b
Prashanth Balakrishnan <prashbala13@gmail.com>
2024-02-14 15:16:01 -0500

    added new styling for images

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,29 @@
 import Accordion from "./components/Accordion";
 import "./App.css";
+import womanOnlineDesktop from "../public/images/illustration-woman-online-desktop.svg";
+import womanOnlineMobile from "../public/images/illustration-woman-online-mobile.svg";
+import box from "../public/images/illustration-box-desktop.svg";
 
 const App = () => {
   return (
     <main className="app">
-      <Accordion />
+      <img className="app__box" src={box} alt="box with a @ sign" />
+      <div className="app__image">
+        <img
+          className="app__image--mobile"
+          src={womanOnlineMobile}
+          alt="woman browsing the web online illustration"
+        />
+
+        <img
+          className="app__image--desktop"
+          src={womanOnlineDesktop}
+          alt="woman browsing the web online illustration"
+        />
+      </div>
+      <div className="app__accordion">
+        <Accordion />
+      </div>
     </main>
   );
 };
--- src/components/Accordion.tsx
+++ src/components/Accordion.tsx
@@ -1,25 +1,59 @@
 import data from "../data";
 import { useState } from "react";
+import "./accordion.css";
+import arrow from "../../public/images/icon-arrow-down.svg";
 
 const Accordion = () => {
   const [selected, setSelected] = useState<number | null>(null);
 
   function handleSingleSelection(getCurrentId: number) {
-    setSelected(getCurrentId === selected ? null : getCurrentId);
+    setSelected((prevSelected) =>
+      prevSelected === getCurrentId ? null : getCurrentId
+    );
   }
 
   return (
-    <div>
+    <section className="accordion">
+      <h1 className="accordion__title">FAQ</h1>
       {data.map((dataItem) => (
-        <div key={dataItem.id}>
-          <button onClick={() => handleSingleSelection(dataItem.id)}>
-            <h3>{dataItem.question}</h3>
-          </button>
+        <div className="accordion__item" key={dataItem.id}>
+          <div className="accordion__questionContainer">
+            <div className="accordion__question">
+              <button onClick={() => handleSingleSelection(dataItem.id)}>
+                <h3
+                  className={
+                    selected === dataItem.id
+                      ? "accordion__question--bold"
+                      : "accordion__question--normal"
+                  }
+                >
+                  {dataItem.question}
+                </h3>
+              </button>
+            </div>
+            <img
+              src={arrow}
+              alt="arrow"
+              className={
+                selected === dataItem.id
+                  ? "accordion__arrow up"
+                  : "accordion__arrow"
+              }
+            />
+          </div>
 
-          {selected === dataItem.id && <p>{dataItem.answer}</p>}
+          <p
+            className={`accordion__answer ${
+              selected === dataItem.id ? "show" : ""
+            }`}
+          >
+            {dataItem.answer}
+          </p>
+          <hr className="accordion__divider" />
         </div>
       ))}
-    </div>
+    </section>
   );
 };
+
 export default Accordion;
